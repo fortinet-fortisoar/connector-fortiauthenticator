8cd80c8ef88f78877f782adb5b68d0afa12ca53a
cs-shubham-morankar <smorankar@fortinet.com>
2022-05-16 11:55:32 +0530

    Action Added

--- fortinet-fortiauthenticator/operations.py
+++ fortinet-fortiauthenticator/operations.py
@@ -22,11 +22,11 @@ error_msgs = {
 
 class FortiAuthenticator(object):
     def __init__(self, config):
-        self.server_url = config.get('server', '')
-        if not self.server_url.startswith('https://'):
-            self.server_url = 'https://' + self.server_url
-        if not self.server_url.endswith('/'):
-            self.server_url += '/'
+        server_url = config.get('server', '')
+        if not server_url.startswith('https://') and not server_url.startswith('http://'):
+            self.server_url = 'https://{0}/'.format(server_url)
+        else:
+            self.server_url = server_url + '/'
         self.username = config.get('username')
         self.api_key = config.get('api_key')
         self.verify_ssl = config.get('verify_ssl')
@@ -58,7 +58,7 @@ class FortiAuthenticator(object):
 def _check_health(config):
     fa = FortiAuthenticator(config)
     try:
-        response = fa.make_api_call(endpoint='/api/v1/')
+        response = fa.make_api_call(endpoint='api/v1/')
         if response:
             return True
     except Exception as err:
@@ -72,12 +72,12 @@ def get_params(params):
 
 def get_schema(config, params):
     fa = FortiAuthenticator(config)
-    return fa.make_api_call(endpoint='/api/v1/')
+    return fa.make_api_call(endpoint='api/v1/')
 
 
 def get_users(config, params):
     fa = FortiAuthenticator(config)
-    return fa.make_api_call(endpoint='/api/v1/localusers/')
+    return fa.make_api_call(endpoint='api/v1/localusers/')
 
 
 def get_local_user(config, params):
@@ -85,23 +85,28 @@ def get_local_user(config, params):
     params = get_params(params)
     mode = params.get('output_mode', '')
     if mode == 'ID':
-        return fa.make_api_call(endpoint='/api/v1/localusers/' + str(params.get('id')) + '/')
+        return fa.make_api_call(endpoint='api/v1/localusers/{0}/'.format(params.get('id')))
     else:
-        return fa.make_api_call(endpoint='/api/v1/localusers/', params=params)
+        return fa.make_api_call(endpoint='api/v1/localusers/', params=params)
 
 
 def create_local_user(config, params):
     fa = FortiAuthenticator(config)
     params = get_params(params)
-    return fa.make_api_call(endpoint='/api/v1/localusers/', data=json.dumps(params), method='POST')
+    return fa.make_api_call(endpoint='api/v1/localusers/', data=json.dumps(params), method='POST')
 
 
 def update_user_status(config, params):
     fa = FortiAuthenticator(config)
-    id = str(params.get('userid'))
     active = params.get('active')
-    return fa.make_api_call(endpoint='/api/v1/localusers/' + id + '/', data=json.dumps({'active': active}),
+    return fa.make_api_call(endpoint='api/v1/localusers/{0}/'.format(params.get('userid')),
+                            data=json.dumps({'active': active}),
                             method='PATCH')
+
+
+def get_ldap_user_list(config, params):
+    fa = FortiAuthenticator(config)
+    return fa.make_api_call(endpoint='api/v1/ldapusers/')
 
 
 def get_ldap_user(config, params):
@@ -109,17 +114,22 @@ def get_ldap_user(config, params):
     params = get_params(params)
     mode = params.get('output_mode', '')
     if mode == 'ID':
-        return fa.make_api_call(endpoint='/api/v1/ldapusers/' + str(params.get('id')) + '/')
+        return fa.make_api_call(endpoint='api/v1/ldapusers/{0}/'.format(params.get('id')))
     else:
-        return fa.make_api_call(endpoint='/api/v1/ldapusers/', params=params)
+        return fa.make_api_call(endpoint='api/v1/ldapusers/', params=params)
 
 
 def update_ldapuser_status(config, params):
     fa = FortiAuthenticator(config)
-    id = str(params.get('userid'))
     active = params.get('active')
-    return fa.make_api_call(endpoint='/api/v1/ldapusers/' + id + '/', data=json.dumps({'active': active}),
+    return fa.make_api_call(endpoint='api/v1/ldapusers/{0}/'.format(params.get('userid')),
+                            data=json.dumps({'active': active}),
                             method='PATCH')
+
+
+def get_radius_user_list(config, params):
+    fa = FortiAuthenticator(config)
+    return fa.make_api_call(endpoint='api/v1/radiususers/')
 
 
 def get_radius_user(config, params):
@@ -127,22 +137,22 @@ def get_radius_user(config, params):
     params = get_params(params)
     mode = params.get('output_mode', '')
     if mode == 'ID':
-        return fa.make_api_call(endpoint='/api/v1/localusers/' + str(params.get('id')) + '/')
+        return fa.make_api_call(endpoint='api/v1/radiususers/{0}/'.format(params.get('id')))
     else:
-        return fa.make_api_call(endpoint='/api/v1/localusers/', params=params)
+        return fa.make_api_call(endpoint='api/v1/radiususers/', params=params)
 
 
 def update_radiususer_status(config, params):
     fa = FortiAuthenticator(config)
-    id = str(params.get('userid'))
     active = params.get('active')
-    return fa.make_api_call(endpoint='/api/v1/radiususers/' + id + '/', data=json.dumps({'active': active}),
+    return fa.make_api_call(endpoint='api/v1/radiususers/{0}/'.format(params.get('userid')),
+                            data=json.dumps({'active': active}),
                             method='PATCH')
 
 
 def get_userlockout_policy(config, params):
     fa = FortiAuthenticator(config)
-    return fa.make_api_call(endpoint='/api/v1/userlockoutpolicy/')
+    return fa.make_api_call(endpoint='api/v1/userlockoutpolicy/')
 
 
 operations = {
@@ -151,8 +161,10 @@ operations = {
     'get_local_user': get_local_user,
     'create_local_user': create_local_user,
     'update_user_status': update_user_status,
+    'get_ldap_user_list': get_ldap_user_list,
     'get_ldap_user': get_ldap_user,
     'update_ldapuser_status': update_ldapuser_status,
+    'get_radius_user_list': get_radius_user_list,
     'get_radius_user': get_radius_user,
     'update_radiususer_status': update_radiususer_status,
     'get_userlockout_policy': get_userlockout_policy
